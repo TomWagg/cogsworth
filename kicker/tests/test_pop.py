9c702d6500ec0bf903dba57d89fe5a31f70af9d5
Tom Wagg <tomjwagg@gmail.com>
2022-10-09 17:23:27 -0700

    add command to create gaiaunlimited folder

--- kicker/tests/test_pop.py
+++ kicker/tests/test_pop.py
@@ -103,6 +103,7 @@ class Test(unittest.TestCase):
         self.assertTrue(p.classes.shape[0] == p.n_binaries_match)
 
         # test that observable table is done right
+        # but set ignore_extinction=True to avoid downloading a huge data file
         p._observables = p.get_observables(ignore_extinction=True)
 
         # cheat and make sure at least one binary is bright enough
