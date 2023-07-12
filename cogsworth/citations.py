eb4d23bad6bd13a5cc33ae9da43f0274daa4be8a
Tom Wagg <tomjwagg@gmail.com>
2023-07-12 22:30:25 +0200

    start citations file

---
+++ cogsworth/citations.py
@@ -0,0 +1,42 @@
+
+# storage for citations a dictionary of tuples, first element bibtex id(s), second element bibtex
+CITATIONS = {
+    "cogsworth": {
+        "tags": ["cogsworth:joss"],
+        "bibtex": "TODO"
+    },
+    "cosmic": {
+        "tags": ["cosmic:apj"],
+        "bibtex": r"""@ARTICLE{Breivik+2020,
+       author = {{Breivik}, Katelyn and {Coughlin}, Scott and {Zevin}, Michael and {Rodriguez}, Carl L. and {Kremer}, Kyle and et al.},
+        title = "{COSMIC Variance in Binary Population Synthesis}",
+      journal = {\apj},
+     keywords = {Compact binary stars, Interacting binary stars, Stellar populations, Gravitational wave astronomy, 283, 801, 1622, 675, Astrophysics - High Energy Astrophysical Phenomena, Astrophysics - Solar and Stellar Astrophysics},
+         year = 2020,
+        month = jul,
+       volume = {898},
+       number = {1},
+          eid = {71},
+        pages = {71},
+          doi = {10.3847/1538-4357/ab9d85},
+archivePrefix = {arXiv},
+       eprint = {1911.00903},
+ primaryClass = {astro-ph.HE},
+       adsurl = {https://ui.adsabs.harvard.edu/abs/2020ApJ...898...71B},
+      adsnote = {Provided by the SAO/NASA Astrophysics Data System}}"""
+    },
+    "gala": {
+        "tags": ["gala:joss"],
+        "bibtex": r"""@article{gala,
+        doi = {10.21105/joss.00388},
+        url = {https://doi.org/10.21105%2Fjoss.00388},
+        year = 2017,
+        month = {oct},
+        publisher = {The Open Journal},
+        volume = {2},
+        number = {18},
+        author = {Adrian M. Price-Whelan},
+        title = {Gala: A Python package for galactic dynamics},
+        journal = {The Journal of Open Source Software}}"""
+    }
+}
