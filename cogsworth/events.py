657d69d8d76e4e211cc6ced6a35f87dc52e33194
Tom Wagg <tomjwagg@gmail.com>
2023-10-26 10:34:37 -0400

    separate events lists

--- cogsworth/events.py
+++ cogsworth/events.py
@@ -32,7 +32,8 @@ def identify_events(full_bpp, full_kick_info):
     # reduce to just the supernova rows and ensure we have the same length in each table
     assert len(full_kick_info) == len(full_bpp)
 
-    events_list = [None for _ in range(len(bin_nums))]
+    primary_events_list = [None for _ in range(len(bin_nums))]
+    secondary_events_list = [None for _ in range(len(bin_nums))]
     for i, bin_num in enumerate(bin_nums):
         if bin_num not in full_bpp.index:
             continue
@@ -40,51 +41,34 @@ def identify_events(full_bpp, full_kick_info):
         bpp = full_bpp.loc[[bin_num]]
         kick_info = full_kick_info.loc[[bin_num]]
 
+        # for primaries and bound binaries we just need a simple list
+        primary_events_list[i] = [{
+            "time": bpp.iloc[j]["tphys"] * u.Myr,
+            "m_1": bpp.iloc[j]["mass_1"] * u.Msun,
+            "m_2": bpp.iloc[j]["mass_2"] * u.Msun,
+            "a": bpp.iloc[j]["sep"] * u.Rsun,
+            "ecc": bpp.iloc[j]["ecc"],
+            "delta_v_sys_xyz": [kick_info.iloc[j]["delta_vsysx_1"],
+                                kick_info.iloc[j]["delta_vsysy_1"],
+                                kick_info.iloc[j]["delta_vsysz_1"]] * u.km / u.s
+        } for j in range(len(kick_info))]
+
         # check if the the binary is going to disrupt at any point
         it_will_disrupt = (kick_info["disrupted"] == 1.0).any()
 
         # iterate over the kick file and store the relevant information (for both if disruption will occur)
         if it_will_disrupt:
-            events_1, events_2 = [], []
             for j in range(len(kick_info)):
-                default_event = {
+                # for rows including the disruption or after it then the secondary component is what we want
+                col_suffix = "_1" if kick_info.iloc[j]["disrupted"] == 0.0 else "_2"
+                secondary_events_list[i].append({
                     "time": bpp.iloc[j]["tphys"] * u.Myr,
                     "m_1": bpp.iloc[j]["mass_1"] * u.Msun,
                     "m_2": bpp.iloc[j]["mass_2"] * u.Msun,
                     "a": bpp.iloc[j]["sep"] * u.Rsun,
                     "ecc": bpp.iloc[j]["ecc"],
-                    "delta_v_sys_xyz": [kick_info.iloc[j]["delta_vsysx_1"],
-                                        kick_info.iloc[j]["delta_vsysy_1"],
-                                        kick_info.iloc[j]["delta_vsysz_1"]] * u.km / u.s
-                }
-                events_1.append(default_event)
-
-                # for rows including the disruption or after it then the secondary component needs editing
-                if kick_info.iloc[j]["disrupted"] == 1.0:
-                    events_2.append({
-                        "time": bpp.iloc[j]["tphys"] * u.Myr,
-                        "m_1": bpp.iloc[j]["mass_1"] * u.Msun,
-                        "m_2": bpp.iloc[j]["mass_2"] * u.Msun,
-                        "a": bpp.iloc[j]["sep"] * u.Rsun,
-                        "ecc": bpp.iloc[j]["ecc"],
-                        "delta_v_sys_xyz": [kick_info.iloc[j]["delta_vsysx_2"],
-                                            kick_info.iloc[j]["delta_vsysy_2"],
-                                            kick_info.iloc[j]["delta_vsysz_2"]] * u.km / u.s
-                    })
-                else:
-                    # otherwise just append the regular stuff
-                    events_2.append(default_event)
-            events_list[i] = [events_1, events_2]
-        else:
-            # for bound binaries we just need a single list
-            events_list[i] = [{
-                "time": bpp.iloc[j]["tphys"] * u.Myr,
-                "m_1": bpp.iloc[j]["mass_1"] * u.Msun,
-                "m_2": bpp.iloc[j]["mass_2"] * u.Msun,
-                "a": bpp.iloc[j]["sep"] * u.Rsun,
-                "ecc": bpp.iloc[j]["ecc"],
-                "delta_v_sys_xyz": [kick_info.iloc[j]["delta_vsysx_1"],
-                                    kick_info.iloc[j]["delta_vsysy_1"],
-                                    kick_info.iloc[j]["delta_vsysz_1"]] * u.km / u.s
-            } for j in range(len(kick_info))]
-    return events_list
+                    "delta_v_sys_xyz": [kick_info.iloc[j][f'delta_vsysx{col_suffix}'],
+                                        kick_info.iloc[j][f'delta_vsysy{col_suffix}'],
+                                        kick_info.iloc[j][f'delta_vsysz{col_suffix}']] * u.km / u.s
+                })
+    return primary_events_list, secondary_events_list
