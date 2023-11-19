a52cca1fa079eddff897f168b9605b59c322da62
Tom Wagg <tomjwagg@gmail.com>
2023-11-19 12:46:41 -0500

    up to date cartoon

--- examples/plot_cartoon.py
+++ examples/plot_cartoon.py
@@ -9,6 +9,25 @@ import cogsworth
 import matplotlib.pyplot as plt
 import numpy as np
 
+# sphinx_gallery_start_ignore
+plt.rc('font', family='serif')
+plt.rcParams['text.usetex'] = False
+fs = 24
+
+# update various fontsizes to match
+params = {'figure.figsize': (12, 8),
+          'legend.fontsize': 0.7*fs,
+          'axes.labelsize': fs,
+          'xtick.labelsize': 0.9 * fs,
+          'ytick.labelsize': 0.9 * fs,
+          'axes.linewidth': 1.1,
+          'xtick.major.size': 7,
+          'xtick.minor.size': 4,
+          'ytick.major.size': 7,
+          'ytick.minor.size': 4}
+plt.rcParams.update(params)
+# sphinx_gallery_end_ignore
+
 p = cogsworth.pop.Population(100, final_kstar1=[13, 14])
 p.sample_initial_binaries()
 p.perform_stellar_evolution()
