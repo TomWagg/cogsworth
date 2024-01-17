db734f99d3d4f47122184267a911ddd34fc5675b
Tom Wagg <tomjwagg@gmail.com>
2024-01-17 16:39:18 -0500

    some very important parentheses...

--- cogsworth/plot.py
+++ cogsworth/plot.py
@@ -81,8 +81,8 @@ def plot_cmd(pop, m_filter="G", c_filter_1="BP", c_filter_2="RP",
         # loop over bound binaries, disrupted primaries and disrupted secondaries
         for dis, marker, suffix in zip([False, True, True], ["o", "^", "v"], ["1", "1", "2"]):
             # select stars with the right kstar (based on which is brighter)
-            mask = ((pop.final_bpp["kstar_1"] == kstar & ~pop.observables["secondary_brighter"])
-                    | (pop.final_bpp["kstar_2"] == kstar & pop.observables["secondary_brighter"]))
+            mask = (((pop.final_bpp["kstar_1"] == kstar) & ~pop.observables["secondary_brighter"])
+                    | ((pop.final_bpp["kstar_2"] == kstar) & pop.observables["secondary_brighter"]))
 
             # apply disruption mask
             mask = mask & pop.disrupted if dis else mask & ~pop.disrupted
