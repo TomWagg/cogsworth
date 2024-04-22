b628dd71d13162eecd51b34484e183b4dbea7063
Tom Wagg <tomjwagg@gmail.com>
2024-04-22 16:40:31 -0400

    test snapshot prep

--- cogsworth/tests/test_hydro.py
+++ cogsworth/tests/test_hydro.py
@@ -1,9 +1,16 @@
 import unittest
 import cogsworth
 import astropy.units as u
+import os
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class Test(unittest.TestCase):
-    def test_bad_inputs(self):
-        """Ensure failure with bad input"""
-        it_broke = False
+    def test_snapshot_prep_changa(self):
+        snap = cogsworth.hydro.utils.prepare_snapshot(os.path.join(THIS_DIR, "test_data/hydro_test"))
+        self.assertTrue(snap["r"].mean().in_units("kpc") < 0.1)
+
+    def test_snapshot_prep_fire(self):
+        snap = cogsworth.hydro.utils.prepare_snapshot(os.path.join(THIS_DIR, "test_data/hydro_test_like_FIRE"))
+        self.assertTrue(snap["r"].mean().in_units("kpc") < 0.1)
