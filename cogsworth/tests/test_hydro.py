c05ee1f9eb1f81bcfe87e05c606c37236ad25a55
Tom Wagg <tomjwagg@gmail.com>
2024-04-22 16:57:05 -0400

    test the potential calculator

--- cogsworth/tests/test_hydro.py
+++ cogsworth/tests/test_hydro.py
@@ -14,3 +14,9 @@ class Test(unittest.TestCase):
     def test_snapshot_prep_fire(self):
         snap = cogsworth.hydro.utils.prepare_snapshot(os.path.join(THIS_DIR, "test_data/hydro_test_like_FIRE"))
         self.assertTrue(snap["r"].mean().in_units("kpc") < 0.1)
+
+    def test_potential_calculation(self):
+        snap = cogsworth.hydro.utils.prepare_snapshot(os.path.join(THIS_DIR, "test_data/hydro_test"))
+        cogsworth.hydro.potential.get_snapshot_potential(snap, verbose=True, out_path="test_pot.yml")
+        self.assertTrue(os.path.exists("test_pot.yml"))
+        os.remove("test_pot.yml")
