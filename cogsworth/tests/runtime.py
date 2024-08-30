4cbb712894fe6ce90f8f2603c00757b690e88db9
Tom Wagg <tomjwagg@gmail.com>
2024-08-30 12:37:54 -0700

    new runtime version just doing one at a time

--- cogsworth/tests/runtime.py
+++ cogsworth/tests/runtime.py
@@ -9,21 +9,22 @@ import os
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Test the runtime scaling of cogsworth")
     parser.add_argument("-i", "--input", type=str, default=None, help="Input file to load in")
-    parser.add_argument("-n", "--nbin", type=int, nargs="+", default=[10, 1000, 100000],
+    parser.add_argument("-n", "--nbin", type=int, default=10000,
                         help="Number of binaries to simulate")
-    parser.add_argument("-p", "--processes", type=int, nargs="+",
-                        default=[1, 2, 4, 8, 16, 32, 64, 128], help="Number of processes to use")
+    parser.add_argument("-p", "--processes", type=int, default=1,
+                        help="Number of processes to use")
     args = parser.parse_args()
 
-    if args.input is None and os.path.exists("runtime_base.h5"):
-        args.input = "runtime_base.h5"
+    if args.input is None and os.path.exists(f"runtime_base_{args.nbin}.h5"):
+        args.input = f"runtime_base_{args.nbin}.h5"
 
     if args.input is None:
         print("Creating a base population")
-        p = cogsworth.pop.Population(max(args.nbin))
+        p = cogsworth.pop.Population(args.nbin)
         p.sample_initial_binaries()
         p.sample_initial_galaxy()
-        p.save("runtime_base.h5")
+        p = p[:args.nbin]
+        p.save(f"runtime_base_{args.nbin}.h5")
     else:
         # load in a population of pre-sampled binaries
         print("Loading in a base population")
@@ -31,17 +32,12 @@ if __name__ == "__main__":
 
     runtimes = np.zeros((len(args.nbin), len(args.processes)))
 
-    for i, nbin in enumerate(args.nbin):
-        print(f"Running {nbin} binaries")
-        for j, nproc in enumerate(args.processes):
-            print(f"Running with {nproc} processes")
-            p_subset = p[:nbin]
-            p_subset.processes = nproc
-            start = time.time()
-            p_subset.perform_stellar_evolution()
-            p_subset.perform_galactic_evolution(progress_bar=False)
-            end = time.time()
-            runtimes[i, j] = end - start
-            print(f"It took {runtimes[i, j]:1.2f} seconds to run")
+    p.processes = args.processes
+    start = time.time()
+    p.perform_stellar_evolution()
+    p.perform_galactic_evolution(progress_bar=False)
+    end = time.time()
+    runtime = end - start
+    print(f"It took {runtime:1.2f} seconds to run")
 
-            np.save("runtimes.npy", runtimes)
+    np.save(f"runtimes_{args.nbin}_{args.processes}.npy", runtime)
