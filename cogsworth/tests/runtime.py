18416758e47893213e45d0f5e81f3517dbd2c728
Tom Wagg <tomjwagg@gmail.com>
2024-09-03 13:42:09 -0700

    smarter testing, weak scaling, split by subfunction

--- cogsworth/tests/runtime.py
+++ cogsworth/tests/runtime.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
                         help="Number of binaries to simulate")
     parser.add_argument("-p", "--processes", type=int, default=1,
                         help="Number of processes to use")
+    parser.add_argument("-w", "--weak-scaling", action="store_true",
+                        help="Use weak scaling", default=False)
     args = parser.parse_args()
 
     if args.input is None and os.path.exists(f"runtime_base_{args.nbin}.h5"):
@@ -30,12 +32,22 @@ if __name__ == "__main__":
         print("Loading in a base population")
         p = cogsworth.pop.load(args.input, parts=["initial_binaries", "initial_galaxy"])
 
+    # if we're testing weak scaling, we need to copy the population for each process
+    if args.weak_scaling:
+        p = cogsworth.pop.concat([p[:] for _ in range(args.processes)])
+
     p.processes = args.processes
     start = time.time()
     p.perform_stellar_evolution()
+    cosmic_done = time.time()
     p.perform_galactic_evolution(progress_bar=False)
-    end = time.time()
-    runtime = end - start
-    print(f"It took {runtime:1.2f} seconds to run")
+    gala_done = time.time()
 
-    np.save(f"runtimes_{args.nbin}_{args.processes}.npy", runtime)
+    cosmic_time = cosmic_done - start
+    gala_time = gala_done - cosmic_done
+    total_time = gala_done - start
+    print(f"{args.nbin} binaries, with {args.processes}, took {total_time:1.2f} seconds to run ({cosmic_time:1.2f} for cosmic, {gala_time:1.2f} for gala)")
+
+    p.save(f"runtime_{args.nbin}_{args.processes}_pop.h5", overwrite=True)
+
+    np.save(f"runtimes_{args.nbin}_{args.processes}.npy", [cosmic_time, gala_time, total_time])
