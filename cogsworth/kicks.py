04c1a5fb991b91b6ee6d7b526fa955046ac6df26
Tom Wagg <tomjwagg@gmail.com>
2025-10-22 14:13:35 -0400

    change the solver

--- cogsworth/kicks.py
+++ cogsworth/kicks.py
@@ -3,6 +3,17 @@ import quaxed.numpy as jnp
 import jax
 import unxt as u
 
+import diffrax as dfx
+import diffraxtra as dfxtra
+
+solver = dfxtra.DiffEqSolver(
+    solver=dfx.Dopri8(scan_kind="bounded"),
+    stepsize_controller=dfx.PIDController(
+        rtol=1e-5, atol=1e-5, dtmin=0.05, dtmax=None, force_dtmin=True
+    ),
+    max_steps=16**3,
+)
+
 __all__ = ["integrate_pop_with_events"]
 
 @jax.jit
@@ -13,18 +24,18 @@ def _integrate_star_with_events(field, x0, v0, t0, events, tf):
 
     x, v = x0.ustrip(field.units), v0.ustrip(field.units)
 
-    sol = gd.integrate_field(field, (x, v), jnp.stack((t0, events["time"][0])), max_steps=100000)
+    sol = gd.integrate_field(field, (x, v), jnp.stack((t0, events["time"][0])), solver=solver, throw=False)
 
     x, v = sol.ys[0][-1], sol.ys[1][-1]
     v = v + ev_dv_no_units[0]
     sol = gd.integrate_field(
-        field, (x, v), jnp.stack((sol.ts[-1], ev_t_no_units[1])), max_steps=100000
+        field, (x, v), jnp.stack((sol.ts[-1], ev_t_no_units[1])), solver=solver, throw=False
     )
 
     x, v = sol.ys[0][-1], sol.ys[1][-1]
     v = v + ev_dv_no_units[1]
     sol = gd.integrate_field(
-        field, (x, v), jnp.stack((sol.ts[-1], tf.uconvert(field.units).value)), max_steps=100000
+        field, (x, v), jnp.stack((sol.ts[-1], tf.uconvert(field.units).value)), solver=solver, throw=False
     )
     return (sol.ys[0][-1], sol.ys[1][-1])
 
