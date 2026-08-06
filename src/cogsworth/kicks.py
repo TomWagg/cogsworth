b34a65f483a17109df73a30292a30e19ea48b6c1
Tom Wagg <tomjwagg@gmail.com>
2026-08-06 17:14:17 -0400

    mask events that out of range

--- src/cogsworth/kicks.py
+++ src/cogsworth/kicks.py
@@ -96,8 +96,12 @@ def integrate_orbit_with_events(
     # ensure timestep isn't larger than integration time
     dt = min(dt, t2 - t1)
 
+    # mask out any events that occur outside the integration time
+    if events is not None:
+        events = events[(events["tphys"] >= 0) & (events["tphys"] * u.Myr <= t2 - t1)]
+
     # if there are no events then just integrate the whole thing
-    if events is None:
+    if events is None or len(events) == 0:
         try:
             full_orbit = potential.integrate_orbit(
                 w0, t1=t1, t2=t2, dt=dt, Integrator=integrator, Integrator_kwargs=integrator_kwargs,
